d7bc852b1984cfc17f599860025cd7ff02bad590
guldron-edu <58664019+guldron-edu@users.noreply.github.com>
2020-05-19 08:53:01 +0300

    add homework 5

---
+++ js/task3.js
@@ -0,0 +1,38 @@
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    const a = this.items.findIndex(currentValue => currentValue === item);
+    if (a === -1) {
+      console.log('Нет такого товара');
+    } else {
+      this.items.splice(a, 1);
+    }
+  }
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
